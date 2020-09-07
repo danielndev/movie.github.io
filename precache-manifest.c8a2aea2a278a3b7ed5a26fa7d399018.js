62656d588ff9dccf7a7ea722dd1e883061026b16
danielndev <63206357+danielndev@users.noreply.github.com>
2020-09-07 23:19:15 +0100

    Added the built project for store app

---
+++ precache-manifest.c8a2aea2a278a3b7ed5a26fa7d399018.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a76cdacac6a332c9da0e82127da443e",
+    "url": "/store-app/index.html"
+  },
+  {
+    "revision": "ed60cc9f5c6ada39b734",
+    "url": "/store-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "72a034a69ab67d6b1c83",
+    "url": "/store-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ed60cc9f5c6ada39b734",
+    "url": "/store-app/static/js/2.6586bcfc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/store-app/static/js/2.6586bcfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a034a69ab67d6b1c83",
+    "url": "/store-app/static/js/main.a0c39df0.chunk.js"
+  },
+  {
+    "revision": "f78d1df9a3ba186d564d",
+    "url": "/store-app/static/js/runtime-main.f02c9ea1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/store-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
